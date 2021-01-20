f769130558f7d39646bd33cc89eebd79373577a9
Alejandro Merino <alejandro2hpxa@gmail.com>
2021-01-19 20:04:22 -0500

    feat: change model viewer attributes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import Head from "next/head";
 
 const Index = () => (
-  <Flex flexDir="column" align="center" justify="center" height="100vh" w="100vw">
+  <Flex
+    flexDir="column"
+    align="center"
+    justify="center"
+    height="100vh"
+    w="100vw"
+  >
     <Head>
       <script
         type="module"
@@ -14,21 +20,21 @@ const Index = () => (
         src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js"
       ></script>
     </Head>
-      <Heading>Model Viewer Concept</Heading>
-      <Box
-        as={"model-viewer" as React.ElementType<any>}
-        style={{
-          height: "200px",
-          width: "200px",
-        }}
-        data-name="baloon"
-        src="./models/nigger.glb"
-        ar
-        ar-modes="scene-viewer quick-look"
-        camera-controls
-        auto-rotate
-        poster=""
-      />
+    <Heading>Model Viewer Concept</Heading>
+    <Box
+      as={"model-viewer" as React.ElementType<any>}
+      style={{
+        height: "200px",
+        width: "200px",
+      }}
+      data-name="coke"
+      src="./models/nigger.glb"
+      ar
+      ar-modes="webxr scene-viewer quick-look"
+      ar-scale="auto"
+      camera-controls
+      poster=""
+    />
   </Flex>
 );
 
